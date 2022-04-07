2941387fa29225b2f26af171907ba9a5444f4107
xfnw <xfnw@riseup.net>
2022-04-07 19:57:54 -0400

    add title

--- gen.sh
+++ gen.sh
@@ -1,9 +1,10 @@
 
 cd "$1"
 
-git pull 2>/dev/null
+git pull >/dev/null 2>&1
 
-echo "<link rel='stylesheet' href='style.css'/><script src='ts.js'></script>"
+title="$(basename $1) channel status"
+echo "<!DOCTYPE html><title>$title</title><link rel='stylesheet' href='style.css'/><script src='ts.js'></script><h1>$title</h1>"
 
 for branch in $(cat channels); do
 	echo "<div><span class='i' id='i$branch'></span> $branch last updated: <span class='l' id='l$branch'>$(git log -1 --format=%cd $branch)</span></div><script>cl('l$branch','i$branch',$(git log -1 --format=%cd --date=unix $branch))</script>"
