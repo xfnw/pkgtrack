eff9930f4dee36df0f4cadc99a75d0a312674b6c
vulpine <xfnw@riseup.net>
2022-04-08 11:58:57 -0400

    mobile support

--- gen.sh
+++ gen.sh
@@ -4,7 +4,7 @@ cd "$1"
 git pull >/dev/null 2>&1
 
 title="$(basename $1) channel status"
-echo "<!DOCTYPE html><title>$title</title><link rel='stylesheet' href='style.css'/><script src='ts.js'></script><h1>$title</h1>"
+echo "<!DOCTYPE html><title>$title</title><meta name='viewport' content='width=device-width, initial-scale=1'><link rel='stylesheet' href='style.css'/><script src='ts.js'></script><h1>$title</h1>"
 
 for branch in $(cat channels); do
 	echo "<div><span class='i' id='i$branch'></span> $branch last updated: <span class='l' id='l$branch'>$(git log -1 --format=%cd $branch)</span></div><script>cl('l$branch','i$branch',$(git log -1 --format=%cd --date=unix $branch))</script>"
