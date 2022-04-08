cc17694799cffc9afd59b1a8248775c710053b4f
xfnw <xfnw@riseup.net>
2022-04-07 21:04:55 -0400

    do not avoid singular units

--- ts.js
+++ ts.js
@@ -5,26 +5,26 @@ function ts(colorIndicator, datein) {
 
     sh(colorIndicator, seconds);
 
-    if (interval > 1) {
-        return interval + " years";
+    if (interval >= 1) {
+        return interval + " year" + (interval==1?'':'s');
     }
     interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months";
+    if (interval >= 1) {
+        return interval + " month" + (interval==1?'':'s');
     }
     interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days";
+    if (interval >= 1) {
+        return interval + " day" + (interval==1?'':'s');
     }
     interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours";
+    if (interval >= 1) {
+        return interval + " hour" + (interval==1?'':'s');
     }
     interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes";
+    if (interval >= 1) {
+        return interval + " minute" + (interval==1?'':'s');
     }
-    return Math.floor(seconds) + " seconds";
+    return Math.floor(seconds) + " second" + (interval==1?'':'s');
 }
 function sh(colorIndicator, seconds) {
     colorIndicator.style.backgroundColor = "hsl("+160*0.999995**seconds+",100%,40%)";
